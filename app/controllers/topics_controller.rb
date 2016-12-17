15342afffe8187275b48b54d130f7cbbe7d2cec7
ruiqing0706 <2944925501@qq.com>
2016-12-17 20:20:58 +0800

    implement votes desc

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,6 +5,7 @@ class TopicsController < ApplicationController
   # GET /topics.json
   def index
     @topics = Topic.all.sort_by {|topic| topic.votes.count}
+    @topics = @topics.reverse
   end
 
   # GET /topics/1
